008de947630f2aeb77a8f853ab4395030ca62e76
Fede C <federico.cipolletta@diestroweb.com>
2023-07-23 16:14:05 -0300

    Pruebas JS clase

--- js/backNavButtonMobile.js
+++ js/backNavButtonMobile.js
@@ -9,3 +9,13 @@ document.getElementById('back-button').addEventListener('click', function () {
     document.getElementById('back-button').style.display = 'none';
     document.getElementById('menu-toggle').style.display = 'block';
 });
+
+console.log("test");
+
+alert("Hola vieja");
+
+//let num1=prompt("Ingresar numero:");
+let num1=parseFloat(prompt("Ingresar numero:"));
+let num2=parseFloat(prompt("Ingresar numero:"));
+let suma = num1 + num2;
+alert("Valor de usuario: " + suma);
